3ca149b83667b0f96eaed13b171e73861d07c3e9
iewilmaestro <83399918+iewilmaestro@users.noreply.github.com>
2021-11-05 13:31:43 +0700

    Add files via upload

--- Run.php
+++ Run.php
@@ -2,10 +2,10 @@
 error_reporting(0);
 
 /*Convert & crop & edit Media*/
-shell_exec("convert img.png -threshold 80% -gravity Center -crop 250x142+0+10 +repage mg.png");
+shell_exec("convert img.png -threshold 65% -gravity Center -crop 250x142+0+10 +repage mg.png");
 
 /*Membaca text di file mg.png*/
-shell_exec("tesseract mg.png captcha -l eng --oem 3 --oem 0 --psm 4 --psm 5 --psm 6 --dpi 800 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz");
+shell_exec("tesseract mg.png captcha -l eng --oem 3 --psm 4 --psm 5 --psm 6 --dpi 1000 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz");
 
 /*Mengambil text*/
 $captcha = file_get_contents('captcha.txt');
